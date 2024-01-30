acf0332a7753931b9f17b1a9837cb6b46cfdb8d3
Paul Mars <paul.mars@canonical.com>
2024-01-30 10:08:08 +0100

    Do not print germinate command in test

--- internal/statemachine/helper_test.go
+++ internal/statemachine/helper_test.go
@@ -3,7 +3,6 @@ package statemachine
 import (
 	"bytes"
 	"crypto/rand"
-	"fmt"
 	"io"
 	"os"
 	"os/exec"
@@ -635,7 +634,6 @@ func TestGenerateGerminateCmd(t *testing.T) {
 				},
 			}
 			germinateCmd := generateGerminateCmd(imageDef)
-			fmt.Print(germinateCmd)
 
 			if !strings.Contains(germinateCmd.String(), tc.mirror) {
 				t.Errorf("germinate command \"%s\" has incorrect mirror. Expected \"%s\"",
